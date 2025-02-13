a121da2f80207e325d37a7d29312cdc7f4f666c1
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2025-02-13 15:54:00 +0100

    chore: update crates

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,8 +1,8 @@
 #![no_std]
 #![no_main]
 
-use panic_probe as _;
 use defmt_rtt as _;
+use panic_probe as _;
 
 use stm32f4xx_hal as hal;
 
@@ -14,31 +14,32 @@ use fugit::{HertzU32, Rate};
 use heapless::spsc::{self, Consumer, Producer};
 
 use hal::{
-    pac, prelude::*,
     gpio::{Output, PushPull, PC13},
     interrupt,
     otg_fs::{UsbBus, USB},
+    pac,
     pac::TIM1,
+    prelude::*,
     timer::*,
 };
 
 use usb_device::{bus::UsbBusAllocator, prelude::*};
 use usbd_audio::{AudioClass, AudioClassBuilder, StreamConfig, TerminalType};
 
-
 const PWM_FREQ: HertzU32 = Rate::<u32, 1, 1>::kHz(40);
 const AUDIO_QUEUE_SIZE: usize = 4096;
 // The amount of samples with the value of 0 received before turning off the output
 const NO_SIGNAL_SAMPLES: usize = 20000;
 
-type PWMType = PwmHz<TIM1, (ChannelBuilder<TIM1, 0, true>, ChannelBuilder<TIM1, 1, true>)>;
+type PWMType = [ErasedChannel<TIM1>; 2];
 
-static G_PWM: Mutex<RefCell<Option<PWMType>>> = Mutex::new(RefCell::new(None));
+static G_PWM_MANAGER: Mutex<RefCell<Option<PwmHzManager<TIM1>>>> = Mutex::new(RefCell::new(None));
+static G_PWM_CHANNELS: Mutex<RefCell<Option<PWMType>>> = Mutex::new(RefCell::new(None));
 static G_MAX_DUTY: Mutex<OnceCell<u16>> = Mutex::new(OnceCell::new());
 
-static G_USB_DEVICE: Mutex<RefCell<Option<UsbDevice<UsbBus<USB>>>>> =
-    Mutex::new(RefCell::new(None));
 static G_USB_AUDIO: Mutex<RefCell<Option<AudioClass<'static, UsbBus<USB>>>>> =
+    Mutex::new(RefCell::new(None));
+static G_USB_DEVICE: Mutex<RefCell<Option<UsbDevice<UsbBus<USB>>>>> =
     Mutex::new(RefCell::new(None));
 
 static G_AUDIO_QUEUE_PROD: Mutex<RefCell<Option<Producer<'static, i16, AUDIO_QUEUE_SIZE>>>> =
@@ -48,19 +49,16 @@ static G_AUDIO_QUEUE_CONS: Mutex<RefCell<Option<Consumer<'static, i16, AUDIO_QUE
 
 static LED: Mutex<RefCell<Option<PC13<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
 
-fn set_output_state(pwm: &mut PWMType, channel: Channel, enabled: bool) {
+fn set_output_state(pwm: &mut PWMType, enabled: bool) {
     // The output is disabled when the control signals are complementary
     // The primary output is ActiveHigh for C1 and ActiveLow for C2
-    pwm.set_complementary_polarity(
-        channel,
-        match channel {
-            Channel::C1 if enabled => Polarity::ActiveLow,
-            Channel::C1 if !enabled => Polarity::ActiveHigh,
-            Channel::C2 if enabled => Polarity::ActiveHigh,
-            Channel::C2 if !enabled => Polarity::ActiveLow,
-            _ => panic!("Only C1 and C2 are supported"),
-        },
-    );
+    if enabled {
+        pwm[0].set_complementary_polarity(Polarity::ActiveLow);
+        pwm[1].set_complementary_polarity(Polarity::ActiveHigh);
+    } else {
+        pwm[0].set_complementary_polarity(Polarity::ActiveHigh);
+        pwm[1].set_complementary_polarity(Polarity::ActiveLow);
+    }
 }
 
 #[entry]
@@ -92,29 +90,34 @@ fn main() -> ! {
         LED.borrow(cs).replace(Some(led));
     });
 
-    let pwm_channels = (
-        Channel1::new(gpioa.pa8).with_complementary(gpiob.pb13),
-        Channel2::new(gpioa.pa9).with_complementary(gpiob.pb14),
-    );
-    let mut pwm = p.TIM1.pwm_hz(pwm_channels, PWM_FREQ, &clocks);
+    let (mut pwm_manager, (pwm_ch1, pwm_ch2, ..)) = p.TIM1.pwm_hz(PWM_FREQ, &clocks);
+    let mut pwm_channels = [
+        pwm_ch1
+            .with(gpioa.pa8)
+            .with_complementary(gpiob.pb13)
+            .erase(),
+        pwm_ch2
+            .with(gpioa.pa9)
+            .with_complementary(gpiob.pb14)
+            .erase(),
+    ];
 
     // Disable the output on power up
-    set_output_state(&mut pwm, Channel::C1, false);
-    set_output_state(&mut pwm, Channel::C2, false);
+    set_output_state(&mut pwm_channels, false);
 
-    pwm.set_polarity(Channel::C1, Polarity::ActiveHigh);
-    pwm.enable(Channel::C1);
-    pwm.enable_complementary(Channel::C1);
+    pwm_channels[0].set_polarity(Polarity::ActiveHigh);
+    pwm_channels[0].enable();
+    pwm_channels[0].enable_complementary();
 
-    pwm.set_polarity(Channel::C2, Polarity::ActiveLow);
-    pwm.enable(Channel::C2);
-    pwm.enable_complementary(Channel::C2);
+    pwm_channels[1].set_polarity(Polarity::ActiveLow);
+    pwm_channels[1].enable();
+    pwm_channels[1].enable_complementary();
 
-    let max_duty = pwm.get_max_duty();
+    let max_duty = pwm_manager.get_max_duty();
     info!("Max duty: {}", max_duty);
-    pwm.set_duty(Channel::C1, max_duty / 2);
-    pwm.set_duty(Channel::C2, max_duty / 2);
-    pwm.listen(Event::C1);
+    pwm_channels[0].set_duty(max_duty / 2);
+    pwm_channels[1].set_duty(max_duty / 2);
+    pwm_manager.listen(Event::C1);
 
     // Audio
     let (prod, cons) = AUDIO_QUEUE.split();
@@ -132,15 +135,6 @@ fn main() -> ! {
         G_AUDIO_QUEUE_PROD.borrow(cs).replace(Some(prod));
         G_AUDIO_QUEUE_CONS.borrow(cs).replace(Some(cons));
 
-        G_USB_DEVICE.borrow(cs).replace(Some(
-            UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27e0))
-                .strings(&[StringDescriptors::default()
-                    .manufacturer("Orange_Murker")
-                    .product("Parametric Speaker")])
-                .unwrap()
-                .build(),
-        ));
-
         G_USB_AUDIO.borrow(cs).replace(Some(
             AudioClassBuilder::new()
                 .output(
@@ -155,10 +149,21 @@ fn main() -> ! {
                 .build(usb_bus)
                 .unwrap(),
         ));
+
+        G_USB_DEVICE.borrow(cs).replace(Some(
+            UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27e0))
+                .max_packet_size_0(64).unwrap()
+                .strings(&[StringDescriptors::default()
+                    .manufacturer("Orange_Murker")
+                    .product("Parametric Speaker")])
+                .unwrap()
+                .build(),
+        ));
     });
 
     cortex_m::interrupt::free(|cs| {
-        G_PWM.borrow(cs).replace(Some(pwm));
+        G_PWM_MANAGER.borrow(cs).replace(Some(pwm_manager));
+        G_PWM_CHANNELS.borrow(cs).replace(Some(pwm_channels));
         G_MAX_DUTY.borrow(cs).get_or_init(|| max_duty);
     });
 
@@ -178,14 +183,19 @@ fn main() -> ! {
 
 #[interrupt]
 fn TIM1_CC() {
-    static mut PWM: Option<PWMType> = None;
+    static mut PWM_MANAGER: Option<PwmHzManager<TIM1>> = None;
+    static mut PWM_CHANNELS: Option<PWMType> = None;
     static mut MAX_DUTY: Option<u16> = None;
     static mut AUDIO_QUEUE_CONS: Option<Consumer<'static, i16, AUDIO_QUEUE_SIZE>> = None;
     // Disable the output at the start
     static mut ZERO_COUNT: usize = NO_SIGNAL_SAMPLES + 1;
 
-    let pwm =
-        PWM.get_or_insert_with(|| cortex_m::interrupt::free(|cs| G_PWM.borrow(cs).take().unwrap()));
+    let pwm_manager = PWM_MANAGER.get_or_insert_with(|| {
+        cortex_m::interrupt::free(|cs| G_PWM_MANAGER.borrow(cs).take().unwrap())
+    });
+    let pwm_channels = PWM_CHANNELS.get_or_insert_with(|| {
+        cortex_m::interrupt::free(|cs| G_PWM_CHANNELS.borrow(cs).take().unwrap())
+    });
     let max_duty = MAX_DUTY.get_or_insert_with(|| {
         cortex_m::interrupt::free(|cs| *G_MAX_DUTY.borrow(cs).get().unwrap())
     });
@@ -193,7 +203,7 @@ fn TIM1_CC() {
         cortex_m::interrupt::free(|cs| G_AUDIO_QUEUE_CONS.borrow(cs).take().unwrap())
     });
 
-    pwm.clear_flags(Flag::C1);
+    pwm_manager.clear_flags(Flag::C1);
 
     // cortex_m::interrupt::free(|cs| {
     //     let mut led = LED.borrow(cs).take().unwrap();
@@ -217,13 +227,11 @@ fn TIM1_CC() {
         *ZERO_COUNT = ZERO_COUNT.saturating_add(1);
         // Disable the output when there is no signal
         if *ZERO_COUNT > NO_SIGNAL_SAMPLES {
-            set_output_state(pwm, Channel::C1, false);
-            set_output_state(pwm, Channel::C2, false);
+            set_output_state(pwm_channels, false);
         }
     } else {
         // Re-enable the output
-        set_output_state(pwm, Channel::C1, true);
-        set_output_state(pwm, Channel::C2, true);
+        set_output_state(pwm_channels, true);
         *ZERO_COUNT = 0;
     }
 
@@ -231,8 +239,8 @@ fn TIM1_CC() {
 
     // info!("Duty: {}", duty);
 
-    pwm.set_duty(Channel::C1, duty as u16);
-    pwm.set_duty(Channel::C2, duty as u16);
+    pwm_channels[0].set_duty(duty as u16);
+    pwm_channels[1].set_duty(duty as u16);
 
     // cortex_m::interrupt::free(|cs| {
     //     let mut led = LED.borrow(cs).take().unwrap();
@@ -247,12 +255,12 @@ fn OTG_FS() {
     static mut USB_AUDIO: Option<AudioClass<'static, UsbBus<USB>>> = None;
     static mut AUDIO_QUEUE_PROD: Option<Producer<'static, i16, AUDIO_QUEUE_SIZE>> = None;
 
-    let usb_dev = USB_DEVICE.get_or_insert_with(|| {
-        cortex_m::interrupt::free(|cs| G_USB_DEVICE.borrow(cs).take().unwrap())
-    });
-
     let usb_audio = USB_AUDIO.get_or_insert_with(|| {
         cortex_m::interrupt::free(|cs| G_USB_AUDIO.borrow(cs).take().unwrap())
+    });
+
+    let usb_dev = USB_DEVICE.get_or_insert_with(|| {
+        cortex_m::interrupt::free(|cs| G_USB_DEVICE.borrow(cs).take().unwrap())
     });
 
     let queue = AUDIO_QUEUE_PROD.get_or_insert_with(|| {
